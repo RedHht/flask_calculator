568a62541b3b7059a2215a87880d7d7213a6fc2a
Thiago Ianuzzi <thiagoianuzzi4@gmail.com>
2023-02-23 19:55:33 -0300

    Funciones cambiadas por metodos

--- static/all.js
+++ static/all.js
@@ -1,45 +1,52 @@
 
-        let calculo = "";
-        let calculoVisual = "";
+let calculo = "";
+let calculoVisual = "";
 
-        function agregarNumero(valor, valorvisual = "") {
+class calculadora {
 
-            if (valorvisual == "") {
+    agregarNumero(valor, valorvisual = "") {
+        if (valorvisual == "") {
 
-                calculo = calculo + valor;
-                calculoVisual = calculoVisual + valor;
-                document.getElementById("resultado").innerHTML=calculoVisual;
+            calculo = calculo + valor;
+            calculoVisual = calculoVisual + valor;
+            document.getElementById("resultado").innerHTML=calculoVisual;
 
-            } else {
+        } else {
 
-                calculo = calculo + valor;
-                calculoVisual = calculoVisual + valorvisual;
-                document.getElementById("resultado").innerHTML = calculoVisual;
-            }
+            calculo = calculo + valor;
+            calculoVisual = calculoVisual + valorvisual;
+            document.getElementById("resultado").innerHTML = calculoVisual;
         }
 
-        function borrar() {
+    }
 
-            calculo = "";
-            calculoVisual = "";
-            document.getElementById("resultado").innerHTML="0";
+    borrar() {
 
-        }
+        calculo = "";
+        calculoVisual = "";
+        document.getElementById("resultado").innerHTML="0";
 
-        function calcular() {
+    }
 
-            fetch('http://127.0.0.1:5000/calcular', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
+    calcular() {
+
+        fetch('http://127.0.0.1:5000/calcular', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
             },
-                body: JSON.stringify({ "calculo": calculo })
-              })
-                .then(response => response.text())
-                .then(result => {
-                  document.getElementById("resultado").textContent = result;
-                });
-            calculo = "";
-            calculoVisual = "";
-            }
+            body: JSON.stringify({ "calculo": calculo })
+        })
+        .then(response => response.text())
+        .then(result => {document.getElementById("resultado").textContent = result;
+        });
+
+        calculo = "";
+        calculoVisual = "";
+
+    }
+
+}
+
+let calculadoraa = new calculadora;
