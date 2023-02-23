6896f013841eaf209e405222258dde4d14c53466
Thiago Ianuzzi <thiagoianuzzi4@gmail.com>
2023-02-23 17:23:24 -0300

    Archivos base añadidso

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import *
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    print("god")
+    return render_template('index.html')
+
+
+@app.route('/calcular', methods=['POST'])
+def calcular():
+    request_data = request.get_json()
+
+    valor = request_data['calculo']
+
+    print(eval(valor))
+
+    valor = str(eval(valor))
+
+    return valor
